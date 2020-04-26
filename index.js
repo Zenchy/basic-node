5592e86b47fe41f046397a4c7d2de481f24ec7c8
Alvaro Corral <alvaro.corral@innocv.com>
2020-04-26 02:24:19 +0200

    Express included

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const http = require('http');
+// const http = require('http');
 
 const colors = require('colors')
 
-const handleServer = (req, res) => {
-  res.writeHead(200, {'Content-type': 'text/html'})
-  res.write('<h1>Hello World from Nodjs!!!</h1>')
-  res.end()
-}
+// const handleServer = (req, res) => {
+//   res.writeHead(200, {'Content-type': 'text/html'})
+//   res.write('<h1>Hello World from Nodjs!!!</h1>')
+//   res.end()
+// }
 
-const server = http.createServer(handleServer)
+// const server = http.createServer(handleServer)
 
 
-server.listen(3000, () => console.log('Servidor en puerto 3000'.cyan))
+// server.listen(3000, () => console.log('Servidor en puerto 3000'.cyan))
+
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req, res) => {
+  res.send('<h1>HOLA EXPRESS!!</h1>')
+} )
+
+server.listen(3000, () => {
+  console.log('Server on 3000 and express'.red)
+})
